89940bead6f1499b8a32a29237654a5ec02b68f5
t6nesu00 <hlw.zigri@gamil.com>
2024-05-13 22:37:55 +0300

    create stateless table content for entries

--- todolist/src/App.jsx
+++ todolist/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import TodoTable from "./TodoTable"
 
 function App() {
   const [todo, setTodo] = useState({description: "", date: ""});
@@ -24,18 +25,7 @@ function App() {
       <input placeholder='Description' name="description" value={todo.description} onChange={inputChanged} />
       <input placeholder='Date' name="date" value={todo.date} onChange={inputChanged} />
       <button onClick={addTodo}>Add</button>
-      <table>
-        <tbody>
-          {
-            todos.map((todo, index) => 
-            <tr key={index}>
-              <td>{todo.description}</td>
-              <td>{todo.date}</td>
-              <td><button onClick={() => deleteTodo(index)}>Delete</button></td>
-              </tr>)
-          }
-        </tbody>
-      </table>
+      <TodoTable todos={todos} deleteTodo={deleteTodo} />
     </>
   )
 }
